91c7882e4f17fd4e76f02ea66716a5d5562eab36
Madeshvinayagam <madeshv@yahoo.com>
2024-03-17 18:44:14 -0700

    Initial Commit

---
+++ Exercise7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise7
+//
+//  Created by user237599 on 3/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
